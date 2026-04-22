5d89aa092fc443f467bde5a4d5d6451a80cf907f
RishanBaweja <rbaweja@calpoly.edu>
2026-04-22 11:45:04 -0700

    features

--- lwp.c
+++ lwp.c
@@ -14,8 +14,11 @@
 #endif
 
 /* ---- Globals ---- */
-static thread current_thread = NULL;   /* who is running right now */
-static tid_t next_tid = 1;            /* counter for unique tids - 1 for call thrd*/
+/* who is running right now */
+static thread current_thread = NULL;
+
+/* counter for unique tids - 1 for call thrd*/
+static tid_t next_tid = 1;
 
 /* linked list of ALL threads(for tid2thread) */
 static thread all_threads = NULL;
@@ -93,19 +96,6 @@ static struct scheduler rr_publish = {
 scheduler RoundRobin = &rr_publish;
 
 
-/*
-Overall Assignment Goal:
-  - Implement support for threads
-  - Threads execute in the same address space as other threads
-  - We are implementing a user-level thread package
-  - This will be done by taking the orignal system thread that runs main and having it 
-  create an arbitrary # of threads
-  - Generally:
-      - lwp_create will create a thread, but it will not run anything
-        until it is returned to by lwp_yield
-      - lwp_yield yields control of current thread to another thread
-      - lwp_exit terminates curr thread and switches to another, if any
-*/
 
 /*
 struct threadinfo_st{
@@ -114,7 +104,6 @@ struct threadinfo_st{
   int status;
 }
 */
-
 /*
 tid t lwp create(lwpfun function, void *argument);
   - Executes a function with arguments 
@@ -122,8 +111,6 @@ tid t lwp create(lwpfun function, void *argument);
   - NO THREAD is in library
   - FOR FPU DOES THAT COME FROM CREATE?
     - library value
-  - thread’s resources will consist of a context and stack, both initialized
-  - s = mmap(NULL,howbig,PROT READ|PROT WRITE,MAP PRIVATE|MAP ANONYMOUS|MAP STACK,-1,0);
     - howbig = ((rl.rlim_curr + page_size - 1) / page_size) * page_size
     - Round up to the nearest multiple of the page size.
       - page_size = long sysconf(_SC_PAGE_SIZE)
@@ -182,9 +169,10 @@ tid_t lwp_create(lwpfun func, void *arg) {
 
   /* build the fake stack frame */
   unsigned long *sp = (unsigned long *)((char *)thread_stack + stack_size);
-
   sp = (unsigned long *)((unsigned long)sp & ~0xFUL);
 
+  sp--;
+  *sp = 0;
   sp--;
   *sp = (unsigned long) lwp_wrap;   /* return address */
   sp--;
@@ -305,7 +293,6 @@ void lwp_exit(int status) {
 
 /*
 tid t lwp wait(int *status);
-  - waits for a thread to finish and deallocates resources, if no available thread, but still runnable threads, block until there is one
   - return terminated threads in FIFO order
   - returns termination status or NO THREAD
 */
@@ -400,7 +387,6 @@ thread tid2thread(tid_t tid) {
 }
 
 /* lwp_wrap(void);
-  - instead of returning %rax and exit looking at %rdi, we wrap it to make it simpler
  */
 static void lwp_wrap(lwpfun fun, void *arg) {
   int rval = fun(arg);
@@ -410,58 +396,41 @@ static void lwp_wrap(lwpfun fun, void *arg) {
 /*
 void lwp set scheduler(scheduler sched);
   - whatever scheduler is passed in will be new scheduler
-  - old scheduler should transfer its threads to the new scheduler in next() order
     - just call next
   - if scheduler is null, do round robin
 */
+
 void lwp_set_scheduler(scheduler sched) {
-  if (sched == NULL) {
+  if (sched == NULL)
     sched = RoundRobin;
-  }
 
-  /* get threads from old scheduler */
-  thread threads = NULL;
-  thread tail = NULL;
+  scheduler old = current_scheduler;
 
-  if (current_scheduler) {
-    thread t;
-    while ((t = current_scheduler->next()) != NULL) {
-      current_scheduler->remove(t);
-
-      t->sched_one = NULL;
-      if (!threads) {
-        threads = t;
-        tail = t;
-      } else {
-        tail->sched_one = t;
-        tail = t;
-      }
-    }
-    if (current_scheduler->shutdown){
-      current_scheduler->shutdown();
-    }
-  }
-
+  /* init new one first */
   current_scheduler = sched;
-
-  /* initialize if needed */
   if (current_scheduler->init){
     current_scheduler->init();
   }
-
-  /* add all collected threads */
-  while (threads) {
-    thread next = threads->sched_one;
-    threads->sched_one = NULL;
-    current_scheduler->admit(threads);
-    threads = next;
+  /* transfer one at a time: remove from old, admit to new */
+  if (old) {
+    thread t;
+    while ((t = old->next()) != NULL) {
+    old->remove(t);
+    t->sched_one = NULL;
+    t->sched_two = NULL;
+    current_scheduler->admit(t);
   }
-}
+  /* shutdown old last */
+    if (old->shutdown){
+      old->shutdown();
+    }
+  }
+  }
 
-/*
-scheduler lwp get scheduler(void);
-  - Returns the pointer to the current scheduler.
-*/
-scheduler lwp_get_scheduler(void) {
-  return current_scheduler;
+  /*
+  scheduler lwp get scheduler(void);
+    - Returns the pointer to the current scheduler.
+  */
+  scheduler lwp_get_scheduler(void) {
+    return current_scheduler;
 }
